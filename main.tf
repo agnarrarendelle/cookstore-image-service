934d90015c2aede81f661e627ef0150d0e9e5ba4
agnarrarendelle <danny20001102@gmail.com>
2023-11-28 17:46:33 +0800

    set up initial code for creating api gateway

--- main.tf
+++ main.tf
@@ -60,3 +60,81 @@ resource "aws_lambda_function" "lambda" {
     }
   }
 }
+
+# Create REST API
+resource "aws_api_gateway_rest_api" "example" {
+  name        = "example"
+  description = "Example REST API for Lambda"
+}
+
+# Create a resource in the API, this is just a logical container.
+resource "aws_api_gateway_resource" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+  path_part   = "myresource"
+}
+
+# Define a GET method on the above resource.
+resource "aws_api_gateway_method" "example" {
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  resource_id   = aws_api_gateway_resource.example.id
+  http_method   = "GET"
+  authorization = "NONE"
+  api_key_required = true
+}
+
+# Connect the Lambda function to the GET method via an integration.
+resource "aws_api_gateway_integration" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_resource.example.id
+  http_method = aws_api_gateway_method.example.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+# Allow the API to trigger the Lambda function.
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/${aws_api_gateway_method.example.http_method}${aws_api_gateway_resource.example.path}"
+}
+
+# The Deploy stage of the API.
+resource "aws_api_gateway_deployment" "example" {
+  depends_on = [aws_api_gateway_integration.example]
+
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  stage_name  = "test"
+  description = "This is a test"
+
+  variables = {
+    "lambdaFunctionName" = aws_lambda_function.lambda.function_name
+  }
+}
+
+resource "aws_api_gateway_api_key" "demo_api_key" {
+  name = "demo_api_key"
+}
+
+resource "aws_api_gateway_usage_plan" "demo_api_usage_plan" {
+  name = "demo_usage_plan"
+
+  api_stages {
+    api_id = aws_api_gateway_rest_api.example.id
+    stage  = "${aws_api_gateway_deployment.example.stage_name}"
+  }
+}
+
+resource "aws_api_gateway_usage_plan_key" "demo_api_usage_plan_key" {
+  key_id        = "${aws_api_gateway_api_key.demo_api_key.id}"
+  key_type      = "API_KEY"
+  usage_plan_id = "${aws_api_gateway_usage_plan.demo_api_usage_plan.id}"
+}
+
+
+
