ccf6d9fcb8c3f0abd2ff865216de51fa1ef147fb
agnarrarendelle <danny20001102@gmail.com>
2023-11-30 02:18:54 +0800

    changed some resources' names

--- main.tf
+++ main.tf
@@ -96,11 +96,11 @@ resource "aws_api_gateway_resource" "product_images" {
   path_part   = "product-images"
 }
 
-# Define a GET method on the above resource.
-resource "aws_api_gateway_method" "cookstore_api_get_method" {
+# Define a POST method on the above resource.
+resource "aws_api_gateway_method" "cookstore_api_post_method" {
   rest_api_id   = aws_api_gateway_rest_api.cookstore_api.id
   resource_id   = aws_api_gateway_resource.product_images.id
-  http_method   = "GET"
+  http_method   = "POST"
   authorization = "NONE"
   api_key_required = true
 }
@@ -109,7 +109,7 @@ resource "aws_api_gateway_method" "cookstore_api_get_method" {
 resource "aws_api_gateway_integration" "cookstore_api_get_method_integration" {
   rest_api_id = aws_api_gateway_rest_api.cookstore_api.id
   resource_id = aws_api_gateway_resource.product_images.id
-  http_method = aws_api_gateway_method.cookstore_api_get_method.http_method
+  http_method = aws_api_gateway_method.cookstore_api_post_method.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -123,7 +123,7 @@ resource "aws_lambda_permission" "apigw" {
   function_name = aws_lambda_function.lambda.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_api_gateway_rest_api.cookstore_api.execution_arn}/*/${aws_api_gateway_method.cookstore_api_get_method.http_method}${aws_api_gateway_resource.product_images.path}"
+  source_arn = "${aws_api_gateway_rest_api.cookstore_api.execution_arn}/*/${aws_api_gateway_method.cookstore_api_post_method.http_method}${aws_api_gateway_resource.product_images.path}"
 }
 
 # The Deploy stage of the API.
@@ -152,7 +152,7 @@ resource "aws_api_gateway_usage_plan" "api_usage_plan" {
   }
 }
 
-resource "aws_api_gateway_usage_plan_key" "demo_api_usage_plan_key" {
+resource "aws_api_gateway_usage_plan_key" "api_usage_plan_key" {
   key_id        = "${aws_api_gateway_api_key.api_key.id}"
   key_type      = "API_KEY"
   usage_plan_id = "${aws_api_gateway_usage_plan.api_usage_plan.id}"
