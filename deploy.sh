b88fd21970d8c5d9692d6e25130cd12ed1cb496c
agnarrarendelle <danny20001102@gmail.com>
2023-12-01 16:53:38 +0800

    removed the commands to zip the compiled code

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 function deploy {
   # Generate a version number based on a date timestamp so that it's unique
   TIMESTAMP=$(date +%Y%m%d%H%M%S)
-  rm -rf code.zip && \
+  rm -rf app.zip && \
   cd lambda/ && \
   rm -rf dist && \
+  rm -rf app.js && \
 
   # Run the npm commands to transpile the TypeScript to JavaScript
   npm i && \
@@ -13,11 +14,11 @@ function deploy {
   # AWS Lambda does not have a use for a package.json or typescript files on runtime.
   mkdir dist &&\
   cp -r ./*.js dist/ &&\
-  cp -r ./node_modules dist/ &&\
-  cd dist &&\
-  find . -name "*.zip" -type f -delete && \
-  # Zip everything in the dist folder and
-  zip -r ../../code.zip .
+  cp -r ./node_modules dist/
+#  cd dist &&\
+#  find . -name "*.zip" -type f -delete && \
+#  # Zip everything in the dist folder and
+#  zip -r ../../code.zip .
 #  cd ../terraform && \
 #  terraform plan -input=false -var lambdasVersion="$TIMESTAMP" -out=./tfplan && \
 #  terraform apply -input=false ./tfplan
